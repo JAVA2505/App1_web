97695e03f79841b46b275edac5665be4df1d1edc
JAVA2505 <carelcontrols@ukr.net>
2018-10-26 20:54:22 +0300

    Spring mvc added

---
+++ src/main/java/controller/AuthController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class AuthController {
+    @GetMapping
+    public ModelAndView auth() {
+        return new ModelAndView("auth");
+    }
+}
