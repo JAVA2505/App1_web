43f0e75f6cf79dabc8729ee9608b2ffa9deed041
JAVA2505 <carelcontrols@ukr.net>
2018-11-01 19:16:44 +0200

    Is working2

--- src/main/java/controller/AccountController.java
+++ src/main/java/controller/AccountController.java
@@ -3,6 +3,7 @@ package controller;
 import dao.UserDao;
 import entity.User;
 import html.HtmlFormer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
